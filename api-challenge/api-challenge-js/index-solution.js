6d9ebc74d4f05d09c35ddd4badf1203aa42167e4
Guy Livni <guylivni@gmail.com>
2020-03-26 14:09:51 +0100

    Suggest JS API challenge changes

--- api-challenge/api-challenge-js/index-solution.js
+++ api-challenge/api-challenge-js/index-solution.js
@@ -1,38 +1,24 @@
-// instructions
-// use the URL https://jsonmock.hackerrank.com/api/movies/search/?Title=[substr] to fetch data about movies of any Harry Potter movies
-// Hint replace the REPLACE_SUBSTR text in the url
-// print to the console and object containing the number of movies returned, and the titles in an array
-// to get started, run 'npm install' to install the node-fetch library (feel free to use any other HTTP request library)
-// to run your code, run 'node index.js' from the console
-
-// Examle substring input: harry potter
-/* Example Output: 
-[
-  { title: 'Harry Potter and the Deathly Hallows: Part 2', year: 2011 },
-  { title: "Harry Potter and the Sorcerer's Stone", year: 2001 },
-  { title: 'Harry Potter and the Chamber of Secrets', year: 2002 },
-  { title: 'Harry Potter and the Goblet of Fire', year: 2005 },
-  { title: 'Harry Potter and the Prisoner of Azkaban', year: 2004 },
-  { title: 'Harry Potter and the Order of the Phoenix', year: 2007 },
-  { title: 'Harry Potter and the Deathly Hallows: Part 1', year: 2010 },
-  { title: 'Harry Potter and the Half-Blood Prince', year: 2009 },
-  { title: 'Harry Potter and the Chamber of Secrets', year: 2002 },
-  { title: 'Harry Potter and the Order of the Phoenix', year: 2007 }
-]
-*/
-
 const fetch = require("node-fetch");
-const url =
-  "https://jsonmock.hackerrank.com/api/movies/search/?Title=harry%20potter";
-fetch(url)
-  .then(res => res.json())
+
+const mapMovies = ({ data }) =>
+  data.map(x => ({
+    title: x.Title,
+    year: x.Year
+  }));
+
+const create_url = term =>
+  `https://jsonmock.hackerrank.com/api/movies/search/?Title=${term}`;
+const apiCallA = fetch(create_url("Deathly")).then(res => res.json());
+const apiCallB = fetch(create_url("Goblet")).then(res => res.json());
+
+Promise.all([apiCallA, apiCallB])
   .then(res => {
-    if (res && res.data) {
-      let newData = res.data.map(x => {
-        return { title: x.Title, year: x.Year };
-      });
-      console.log(newData);
-    }
+    const result = res.reduce((acc, currRes) => {
+      const mappedMovies = mapMovies(currRes);
+      return acc.concat(mappedMovies);
+    }, []);
+
+    console.log(result);
   })
   .catch(err => {
     console.log("error: " + err);
--- api-challenge/api-challenge-js/index.js
+++ api-challenge/api-challenge-js/index.js
@@ -1,25 +1,31 @@
-// Instructions
-// use the URL https://jsonmock.hackerrank.com/api/movies/search/?Title=[substr] to fetch data about movies
-// Hint replace the REPLACE_SUBSTR text in the url
-// print to the console and object containing the number of movies returned, and the titles in an array
-// to get started, run 'npm install' to install the node-fetch library (feel free to use any other HTTP request library)
-// to run your code, run 'node index.js' from the console
-// if you cant get the api call to work or the results, write down in psuedo code the steps you would take in order to get the desired output
-
-// Examle substring input: harry potter
-/* Example Output: 
-[
-  { title: 'Harry Potter and the Deathly Hallows: Part 2', year: 2011 },
-  { title: "Harry Potter and the Sorcerer's Stone", year: 2001 },
-  { title: 'Harry Potter and the Chamber of Secrets', year: 2002 },
-  { title: 'Harry Potter and the Goblet of Fire', year: 2005 },
-  { title: 'Harry Potter and the Prisoner of Azkaban', year: 2004 },
-  { title: 'Harry Potter and the Order of the Phoenix', year: 2007 },
-  { title: 'Harry Potter and the Deathly Hallows: Part 1', year: 2010 },
-  { title: 'Harry Potter and the Half-Blood Prince', year: 2009 },
-  { title: 'Harry Potter and the Chamber of Secrets', year: 2002 },
-  { title: 'Harry Potter and the Order of the Phoenix', year: 2007 }
-]
+/** 
+* - Instructions -
+* Use the URL https://jsonmock.hackerrank.com/api/movies/search/?Title=[substr] to make 2 API calls about movies.
+* Hint: replace the REPLACE_SUBSTR text in the url
+* Combine the result of the 2 API calls, and map it into the following structure:
+* [
+*   {title, year},
+*   {title, year},
+*   {title, year},
+* ]
+* print the mapped result to the console
+*
+* - Getting Started -
+* run 'npm install' to install the node-fetch library (feel free to use any other HTTP request library)
+* to run your code, run 'node index.js' from the console
+* if you cant get the api call to work or the results, 
+* write down in psuedo code the steps you would take in order to get the desired output
+*
+* - Example -
+* Substring input 1: Deathly
+* Substring input 1: Goblet
+* Expected Output:
+* [
+*   { title: 'Harry Potter and the Deathly Hallows: Part 1', year: 2010 },
+*   { title: 'Harry Potter and the Deathly Hallows: Part 2', year: 2011 }
+*   { title: 'Harry Potter and the Goblet of Fire', year: 2005 },
+*   { title: "Inside 'Harry Potter and the Goblet of Fire'", year: 2005 }
+* ]
 */
 
 const fetch = require("node-fetch");
